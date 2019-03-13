51792fcb515c78416ac8434665b42e5b0f5ec3fe
Maxi Learner <sagmeister@gmail.com>
2019-03-13 14:54:10 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,22 +1,19 @@
-// function forLoop(array) {
-//   for (var i = 0;  i < 25; i++) {
-//     if (i === 1) {
-//       array.push("I am 1 strange loop.")
-//     }
-//     else {
-//       array.push(`I am ${i} strange loops.`)
-//     }
-//   }
-// return array
-// }
+function forLoop(array) {
+  for (var i = 0;  i < 25; i++) {
+    if (i === 1) {
+      array.push("I am 1 strange loop.")
+    }
+    else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+return array
+}
 
 
 function whileLoop(n) {
-  // var n = 27
   while (n > 0) {
     console.log(--n)
   }
-  // for (n === 0) {
   return "done"
-  // }
 }
